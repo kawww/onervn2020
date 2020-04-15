430170422134c1e40acce165f66aa82aab99337e
kawww <39939137+kawww@users.noreply.github.com>
2020-04-15 11:43:53 +0800

    Create move.php

---
+++ tool/move.php
@@ -0,0 +1,289 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>EXPLORER</title>
+		<style>
+
+
+html,
+body,
+.site_font {
+  font-family: coda_regular, arial, helvetica, sans-serif;
+}
+
+
+
+html, body {
+  background-color: #0b0c0d;
+  color: #fff;
+  font-size: 15px;
+  margin: 0 auto -100px;
+  padding: 0;
+
+}
+
+::-webkit-scrollbar { width: 0 !important }
+
+
+
+a:link,
+a:visited,
+a:active{
+ transition:0.5s;
+color: #28f428;	
+  text-decoration: none;
+}
+
+a:hover { color:yellow; }
+
+input[type="text"],
+input[type="submit"] {
+  font-size: 18px;
+}
+
+
+
+input[type="text"],
+input[type="submit"] {
+  border: 1px solid #59fbea;
+  font-family: coda_regular, arial, helvetica, sans-serif;
+  -webkit-appearance: none;
+  -webkit-border-radius: 0;
+  height:42px;
+ 
+
+}
+
+input[type="text"] {
+  background-color: rgb(11, 12, 13);
+  color: #ddd;
+
+ width:500px;
+ padding-left:5px;
+
+}
+
+input[type="submit"] {
+  background-color: rgb(0, 79, 74);
+  color: #59fbea;
+  padding: 5px 22px;
+margin-left:3px;
+  height:45px;
+    
+}
+
+div{margin:5px;border:0;padding:0;}
+
+#door {
+
+margin-top:15px;
+  
+  font-size: 15px;
+  
+
+}
+
+#newworld{
+
+margin-top:100px;
+  
+  font-size: 15px;
+  
+
+}
+
+
+
+
+
+#tech {
+
+  
+margin-left: 11px;
+padding-left: 2px;
+text-align: left;
+vertical-align:middle;
+
+  border: 0px solid #59fbea;
+  font-family: coda_regular, arial, helvetica, sans-serif;
+  -webkit-appearance: none;
+  -webkit-border-radius: 0;
+  font-size:24px;
+
+width:98%;
+
+ 
+  
+}
+
+@keyframes textShadow {
+  0% {
+    text-shadow: 0.4389924193300864px 0 1px rgba(0,30,255,0.5), -0.4389924193300864px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+
+  100% {
+    text-shadow: 2.6208764473832513px 0 1px rgba(0,30,255,0.5), -2.6208764473832513px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+  }
+}
+
+.crt::before {
+  content: " ";
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: linear-gradient(rgba(18, 16, 16, 0) 80%, rgba(0, 0, 0, 0.25) 50%), linear-gradient(90deg, rgba(255, 0, 0, 0.06), rgba(0, 255, 0, 0.02), rgba(0, 0, 255, 0.06));
+  z-index: 2;
+  background-size: 100% 2px;
+  pointer-events: none;
+}
+.crt {
+  animation: textShadow 0.00s infinite;
+}
+
+            #nav
+            {
+                /*width: 80%;*/
+                margin: 0 auto;
+				margin-left: 13px;
+				padding-left: 3px;
+                border: 0px solid #59fbea;
+            }
+            ul,li 
+            {
+                margin: 0px;
+                padding: 0px;
+                list-style: none;
+            }
+            ul 
+            {
+                display: flex;
+                flex-direction: row;
+                flex-wrap: wrap;
+
+
+            }
+
+			div:before {
+  content:"";
+  position:absolute;
+  top:0px;
+  bottom:0;
+  left:0;
+  right:0;
+  background:linear-gradient(to bottom,transparent,#000 0px);
+  animation:fadeIn 1s forwards
+}
+
+@keyframes fadeIn {
+  0% {
+    top:5%;
+  }
+  100% {
+    top: 100%;
+  }
+}
+
+            li
+            {
+                border: 1px solid #59fbea;
+                width: 430px;
+				height:100px;
+				word-break: break-all;
+			background-color: rgb(0, 79, 74);
+                text-align: center;
+                margin-top: 10px;
+                margin-bottom: 7px;
+				margin-right: 5px;
+				margin-left: 5px;
+				padding-top:10px;
+				padding-left:2px;
+				padding-right:2px;
+                flex:auto;  
+				
+
+            }
+#universe {
+
+line-height:26px;
+ont-weight:100px;
+font-size: 22px;
+position: absolute;
+  
+}
+
+p
+{
+
+color:#ccc;
+margin-top:2px;
+
+}
+
+</style>
+		<body>
+
+		
+
+
+<?php 
+
+//move assets
+
+$address="";
+
+
+//check server
+
+include("rpc.php");
+
+$kpc = new Keva();
+
+$kpc->username=$krpcuser;
+$kpc->password=$krpcpass;
+$kpc->host=$krpchost;
+$kpc->port=$krpcport;
+
+$rpc = new Raven();
+
+$rpc->username=$rrpcuser;
+$rpc->password=$rrpcpass;
+$rpc->host=$rrpchost;
+$rpc->port=$rrpcport;
+
+
+//rpc
+
+
+$assetlist=$rpc->listmyassets();
+
+foreach($assetlist as $asset=>$num)
+
+		{
+
+
+			
+					echo $asset;
+
+					echo $num;
+					echo "<br><br>";
+
+					$tran=$rpc->transfer($asset,$num,$address);
+
+					print_r($tran);
+
+
+				
+		}
+
+
+
+
+?>
+</ul></div>
+</div>
+</body>
+</html>
